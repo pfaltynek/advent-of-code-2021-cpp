a1d346edbf14cc8fe99b735dec44917ca1d13aed
Petr Faltýnek <pfaltynek@gmail.com>
2021-12-01 16:55:24 +0100

    day01 finished

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,5 +1,7 @@
 #include "./../common/aoc.hpp"
 
+const size_t C_SUM_WINDOW_SIZE = 3;
+
 class AoC2021_day01 : public AoC {
   protected:
 	bool init(const std::vector<std::string> lines);
@@ -11,6 +13,7 @@ class AoC2021_day01 : public AoC {
 
   private:
 	int64_t get_depth_increases_count();
+	int64_t get_depth_sum_increases_count();
 	std::vector<uint16_t> depths_;
 };
 
@@ -41,6 +44,35 @@ int64_t AoC2021_day01::get_depth_increases_count() {
 	return result;
 }
 
+int64_t AoC2021_day01::get_depth_sum_increases_count() {
+	int64_t result = 0, prev_sum = 0, sum;
+
+	if (depths_.size() < 3) {
+		return 0;
+	}
+
+	for (size_t i = 0; i < C_SUM_WINDOW_SIZE; i++) {
+		prev_sum += depths_[i];
+	}
+
+	for (size_t i = 1; i < depths_.size() + 1 - C_SUM_WINDOW_SIZE; i++) {
+
+		sum = 0;
+
+		for (size_t j = 0; j < C_SUM_WINDOW_SIZE; j++) {
+			sum += depths_[i + j];
+		}
+
+		if (sum > prev_sum) {
+			result++;
+		}
+
+		prev_sum = sum;
+	}
+
+	return result;
+}
+
 int32_t AoC2021_day01::get_aoc_day() {
 	return 1;
 }
@@ -53,7 +85,8 @@ void AoC2021_day01::tests() {
 	int64_t result;
 
 	if (init({"199", "200", "208", "210", "200", "207", "240", "269", "260", "263"})) {
-		result = get_depth_increases_count(); // 7
+		result = get_depth_increases_count();	  // 7
+		result = get_depth_sum_increases_count(); // 5
 	}
 }
 
@@ -64,13 +97,16 @@ bool AoC2021_day01::part1() {
 
 	result1_ = std::to_string(result);
 
-	return false;
+	return true;
 }
 
 bool AoC2021_day01::part2() {
 	int64_t result = 0;
 
+	result = get_depth_sum_increases_count();
+
 	result2_ = std::to_string(result);
+
 	return true;
 }
 
