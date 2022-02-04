5a63d2eb5a3f0edb3bc5934a9ba6aa8f297a8917
Petr Faltýnek <pfaltynek@gmail.com>
2022-02-04 16:29:35 +0100

    day16 - part1 finished

---
+++ day16/main.cpp
@@ -0,0 +1,233 @@
+#include "./../common/aoc.hpp"
+
+class bits_stream {
+  public:
+	bits_stream(const std::vector<uint8_t> message) {
+		stream_ = message;
+		remaining_bits_ = 8;
+		index_ = 0;
+		ver_sum_ = 0;
+		bits_read_ = 0;
+	}
+
+	uint32_t get_version_sum() const {
+		return ver_sum_;
+	}
+
+	void decode_stream() {
+		decode_packet();
+	}
+
+  private:
+	void decode_packet() {
+		uint32_t version, type_id, literal_value, length_type_id, count, bits_count;
+
+		version = get_bits(3);
+		ver_sum_ += version;
+		type_id = get_bits(3);
+
+		switch (type_id) {
+			case 4: // literal value
+				literal_value = decode_literal();
+				break;
+			default:
+				length_type_id = get_bits(1);
+
+				if (length_type_id == 0) {
+					count = get_bits(15);
+					bits_count = bits_read_;
+
+					while (bits_count + count > bits_read_) {
+						decode_packet();
+					}
+				} else {
+					count = get_bits(11);
+
+					for (size_t i = 0; i < count; i++) {
+						decode_packet();
+					}
+				}
+				break;
+		}
+	}
+
+	uint32_t decode_literal() {
+		uint32_t start_bit = 0, value, result = 0;
+
+		do {
+			start_bit = get_bits(1);
+			value = get_bits(4);
+			result = result << 4;
+			result |= value;
+		} while (start_bit != 0);
+
+		return result;
+	}
+
+	bits_stream() {
+	}
+
+	static void mask_bits(const uint8_t bits_count, uint32_t& data) {
+		uint32_t mask = (1 << bits_count) - 1;
+		data &= mask;
+	}
+
+	uint32_t get_bits(const uint8_t bits_count) {
+		uint8_t cnt = bits_count % 32;
+		uint32_t result = 0, tmp, bits_read = 0;
+
+		while ((cnt > 0) && (index_ < stream_.size())) {
+			if (cnt < remaining_bits_) {
+				result = result << cnt;
+				tmp = stream_[index_];
+				tmp = tmp >> (remaining_bits_ - cnt);
+				mask_bits(cnt, tmp);
+				result |= tmp;
+
+				remaining_bits_ -= cnt;
+				bits_read += cnt;
+				cnt = 0;
+			} else {
+				result = result << remaining_bits_;
+				tmp = stream_[index_];
+				mask_bits(remaining_bits_, tmp);
+				result |= tmp;
+
+				cnt -= remaining_bits_;
+				bits_read += remaining_bits_;
+				remaining_bits_ = 8;
+				index_++;
+			}
+		}
+
+		bits_read_ += bits_read;
+
+		return result;
+	}
+
+	std::vector<uint8_t> stream_;
+	uint8_t remaining_bits_;
+	size_t index_, ver_sum_, bits_read_;
+};
+
+class AoC2021_day16 : public AoC {
+  protected:
+	bool init(const std::vector<std::string> lines);
+	bool part1();
+	bool part2();
+	void tests();
+	int32_t get_aoc_day();
+	int32_t get_aoc_year();
+
+  private:
+	std::vector<uint8_t> message_;
+};
+
+bool AoC2021_day16::init(const std::vector<std::string> lines) {
+	size_t index = 0;
+	std::string byte, msg;
+	unsigned long hexa;
+
+	message_.clear();
+	msg.clear();
+
+	for (uint32_t i = 0; i < lines.size(); i++) {
+		msg.append(lines[i]);
+	}
+
+	while (index < msg.size()) {
+		byte = msg.substr(index, 2);
+		index += 2;
+
+		if (byte.size() < 2) {
+			byte.append("0");
+		}
+
+		hexa = std::stoul(byte, nullptr, 16);
+		message_.push_back(static_cast<uint8_t>(hexa));
+	}
+
+	return true;
+}
+
+int32_t AoC2021_day16::get_aoc_day() {
+	return 16;
+}
+
+int32_t AoC2021_day16::get_aoc_year() {
+	return 2021;
+}
+
+void AoC2021_day16::tests() {
+	int64_t result;
+
+	if (init({"D2FE28"})) {
+		bits_stream bs(message_);
+		bs.decode_stream();
+		result = bs.get_version_sum(); // ?
+	}
+
+	if (init({"38006F45291200"})) {
+		bits_stream bs(message_);
+		bs.decode_stream();
+		result = bs.get_version_sum(); // ?
+	}
+
+	if (init({"EE00D40C823060"})) {
+		bits_stream bs(message_);
+		bs.decode_stream();
+		result = bs.get_version_sum(); // ?
+	}
+
+	if (init({"8A004A801A8002F478"})) {
+		bits_stream bs(message_);
+		bs.decode_stream();
+		result = bs.get_version_sum(); // 16
+	}
+
+	if (init({"620080001611562C8802118E34"})) {
+		bits_stream bs(message_);
+		bs.decode_stream();
+		result = bs.get_version_sum(); // 12
+	}
+
+	if (init({"C0015000016115A2E0802F182340"})) {
+		bits_stream bs(message_);
+		bs.decode_stream();
+		result = bs.get_version_sum(); // 23
+	}
+
+	if (init({"A0016C880162017C3686B18A3D4780"})) {
+		bits_stream bs(message_);
+		bs.decode_stream();
+		result = bs.get_version_sum(); // 31
+	}
+}
+
+bool AoC2021_day16::part1() {
+	int64_t result = 0;
+
+	bits_stream bs(message_);
+	bs.decode_stream();
+	result = bs.get_version_sum();
+
+	result1_ = std::to_string(result);
+
+	return true;
+}
+
+bool AoC2021_day16::part2() {
+	int64_t result = 0;
+
+	result = 0;
+
+	result2_ = std::to_string(result);
+
+	return true;
+}
+
+int main(void) {
+	AoC2021_day16 day16;
+
+	return day16.main_execution();
+}
