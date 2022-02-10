33b2721f273872909be69a9d15ba7f39df82d8a6
Petr Faltýnek <pfaltynek@gmail.com>
2022-02-10 11:15:54 +0100

    day17 finished

--- day17/main.cpp
+++ day17/main.cpp
@@ -16,10 +16,12 @@ class AoC2021_day17 : public AoC {
 
   private:
 	int32_t target_x1_, target_x2_, target_y1_, target_y2_;
-	int32_t get_highest_y_position() const;
+	std::vector<int32_t> velx_;
+	int32_t get_highest_y_position();
+	int32_t get_possible_hit_count();
 	int32_t get_next_x_velocity(const int32_t x_velocity) const;
 	int32_t get_next_y_velocity(const int32_t y_velocity) const;
-	int32_t get_highest_y_position(const int32_t x_vel, const int32_t y_vel) const;
+	bool check_probe_target_hit(int32_t x_vel, int32_t y_vel, int32_t& highest_y_position) const;
 };
 
 bool AoC2021_day17::init(const std::vector<std::string> lines) {
@@ -41,31 +43,63 @@ bool AoC2021_day17::init(const std::vector<std::string> lines) {
 	return false;
 }
 
-int32_t AoC2021_day17::get_highest_y_position() const {
-	std::vector<int32_t> velx = {};
+int32_t AoC2021_day17::get_highest_y_position() {
 	int32_t tmp = 0, inc = 1, max_y = 0;
+
+	velx_.clear();
 
 	// get max x velocity to fit target:
 	while (tmp <= target_x2_) {
 		tmp += inc;
 
 		if ((tmp <= target_x2_) && (tmp >= target_x1_)) {
-			velx.push_back(inc);
+			velx_.push_back(inc);
 		}
 		inc++;
 	}
 
-	for (size_t i = 0; i < velx.size(); i++) {
+	for (size_t i = 0; i < velx_.size(); i++) {
 		for (size_t j = abs(target_y2_); j <= abs(target_y1_); j++) {
-			tmp = get_highest_y_position(velx[i], j);
-
-			if (tmp > max_y) {
-				max_y = tmp;
+			if (check_probe_target_hit(velx_[i], j, tmp)) {
+				if (tmp > max_y) {
+					max_y = tmp;
+				}
 			}
 		}
 	}
 
 	return max_y;
+}
+
+int32_t AoC2021_day17::get_possible_hit_count() {
+	int32_t xmax = *std::max_element(velx_.begin(), velx_.end());
+	int32_t tmp, velx;
+	int32_t result = 0;
+
+	for (int32_t i = xmax + 1; i <= target_x2_; i++) {
+		tmp = 0;
+		velx = i;
+
+		while (tmp <= target_x2_) {
+			tmp += velx;
+			velx = get_next_x_velocity(velx);
+
+			if ((tmp <= target_x2_) && (tmp >= target_x1_)) {
+				velx_.push_back(i);
+				break;
+			}
+		}
+	}
+
+	for (size_t i = 0; i < velx_.size(); i++) {
+		for (int32_t j = std::min(target_y1_, target_y2_); j <= std::max(abs(target_y2_), abs(target_y1_)); j++) {
+			if (check_probe_target_hit(velx_[i], j, tmp)) {
+				result++;
+			}
+		}
+	}
+
+	return result;
 }
 
 int32_t AoC2021_day17::get_next_x_velocity(const int32_t x_velocity) const {
@@ -82,31 +116,29 @@ int32_t AoC2021_day17::get_next_y_velocity(const int32_t y_velocity) const {
 	return y_velocity - 1;
 }
 
-int32_t AoC2021_day17::get_highest_y_position(int32_t x_vel, int32_t y_vel) const {
-	int32_t max_y = 0, x = 0, y = 0;
-	bool in_target = false;
+bool AoC2021_day17::check_probe_target_hit(int32_t x_vel, int32_t y_vel, int32_t& highest_y_position) const {
+	int32_t x = 0, y = 0;
+	bool result = false;
+
+	highest_y_position = INT32_MIN;
 
 	while ((x <= target_x2_) && (y >= target_y1_)) {
 		x += x_vel;
 		y += y_vel;
 
 		if ((target_x1_ <= x) && (target_x2_ >= x) && (target_y1_ <= y) && (target_y2_ >= y)) {
-			in_target = true;
+			result = true;
 		}
 
-		if (y > max_y) {
-			max_y = y;
+		if (y > highest_y_position) {
+			highest_y_position = y;
 		}
 
 		x_vel = get_next_x_velocity(x_vel);
 		y_vel = get_next_y_velocity(y_vel);
 	}
 
-	if (in_target) {
-		return max_y;
-	} else {
-		return 0;
-	}
+	return result;
 }
 
 int32_t AoC2021_day17::get_aoc_day() {
@@ -122,6 +154,7 @@ void AoC2021_day17::tests() {
 
 	if (init({"target area: x=20..30, y=-10..-5"})) {
 		result = get_highest_y_position(); // 45
+		result = get_possible_hit_count(); // 112
 	}
 }
 
@@ -137,6 +170,8 @@ bool AoC2021_day17::part1() {
 
 bool AoC2021_day17::part2() {
 	int64_t result = 0;
+
+	result = get_possible_hit_count();
 
 	result2_ = std::to_string(result);
 
